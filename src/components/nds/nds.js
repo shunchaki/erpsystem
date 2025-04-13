61855f1c073a1c6f7462571f13359e9686e7676e
Bekmurod <boqiyev482@gmail.com>
2025-04-13 23:11:03 +0500

    updated

--- src/components/nds/nds.js
+++ src/components/nds/nds.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import {useEffect, useState} from 'react';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -7,39 +7,25 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
+import { ApiService } from '../../services/api.service';
 
 const Nds = () => {
 
-    const columns = [
-        { id: 'name', label: 'Название', minWidth: 170 },
-        { id: 'code', label: 'Ставка (%)', minWidth: 100 },
-      ];
-      
+  const [ndsItems,setNdsItem]= useState([])
+  
+      useEffect(()=> {
+        ApiService.fetching('wms/nds/all?Params.PageSize=30&Params.PageIndex=1').then(data => setNdsItem())
+      },[])
       function createData(name, code, population, size) {
         const density = population / size;
         return { name, code, population, size, density };
       }
-      
-      const rows = [
-        createData('India', 'IN'),
-        createData('China', 'CN'),
-        createData('Italy', 'IT'),
-        createData('United States', 'US', 327167434, 9833520),
-        createData('Canada', 'CA', 37602103, 9984670),
-        createData('Australia', 'AU', 25475400, 7692024),
-        createData('Germany', 'DE', 83019200, 357578),
-        createData('Ireland', 'IE', 4857000, 70273),
-        createData('Mexico', 'MX', 126577691, 1972550),
-        createData('Japan', 'JP', 126317000, 377973),
-        createData('France', 'FR', 67022000, 640679),
-        createData('United Kingdom', 'GB', 67545757, 242495),
-        createData('Russia', 'RU', 146793744, 17098246),
-        createData('Nigeria', 'NG', 200962417, 923768),
-        createData('Brazil', 'BR', 210147125, 8515767),
-      ];
-      
-        const [page, setPage] = React.useState(0);
-        const [rowsPerPage, setRowsPerPage] = React.useState(10);
+      const SelectNds = (data) =>{
+        console.log()
+      }
+      console.log(ndsItems,"nds")
+        const [page, setPage] = useState(0);
+        const [rowsPerPage, setRowsPerPage] = useState(10);
       
         const handleChangePage = (event, newPage) => {
           setPage(newPage);
@@ -52,37 +38,24 @@ const Nds = () => {
     
     return (
     <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
+      <TableContainer sx={{ maxHeight: 600 }}>
         <Table stickyHeader aria-label="sticky table">
           <TableHead>
             <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
+              <TableCell>№</TableCell>
+              <TableCell>Название</TableCell>
+              <TableCell>Ставка (%)</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            {ndsItems
+              
               .map((row) => {
                 return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                    {columns.map((column) => {
-                      const value = row[column.id];
-                      return (
-                        <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === 'number'
-                            ? column.format(value)
-                            : value}
-                        </TableCell>
-                      );
-                    })}
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.rate}>
+                    <TableCell>{ndsItems.length}</TableCell>
+                    <TableCell>{row.name}</TableCell>
+                    <TableCell>{row.rate}%</TableCell>
                   </TableRow>
                 );
               })}
@@ -92,7 +65,7 @@ const Nds = () => {
       <TablePagination
         rowsPerPageOptions={[10, 25, 100]}
         component="div"
-        count={rows.length}
+        count={ndsItems.length}
         rowsPerPage={rowsPerPage}
         page={page}
         onPageChange={handleChangePage}
