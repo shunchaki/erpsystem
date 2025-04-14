4722a33885f341027ea0f76ca16b062d076127a6
myusername <myemail>
2025-04-14 21:15:43 +0500

    Add search functionality and modal for MeasureType component; update routing and state management

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
-import Nds from './nds/nds';
-
 export {default as App} from './app/app'
 export {default as MeasureType} from './MeasureTypes/measure-type';
 export {default as Nds} from './nds/nds'
 export {default as Main} from './main/main'
+export {default as ModalMeasureType} from './modal/modal'
